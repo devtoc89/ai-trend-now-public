a91f4d8623cbd19e97ee78a7142643e5ba957344
devtoc89 <devtoc89@gmail.com>
2024-11-19 03:12:57 +0900

    chore: [storybook] initialize project with basic setup

--- apps/storybook/src/Button/Button.stories.ts
+++ apps/storybook/src/Button/Button.stories.ts
@@ -1,6 +1,6 @@
 import { Button, ButtonVariants } from "@repo/ui/organisms";
 import type { Meta, StoryObj } from "@storybook/react";
-import { fn } from "@storybook/test";
+// import { fn } from "@storybook/test";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta: Meta<typeof Button> = {
@@ -22,7 +22,9 @@ const meta: Meta<typeof Button> = {
     onClick: { action: "clicked" },
   },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  args: { onClick: fn() },
+  args: {
+    // onClick: fn()
+  },
 } satisfies Meta<typeof Button>;
 
 export default meta;
