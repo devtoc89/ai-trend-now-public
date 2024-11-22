e9935ce88ea4b7369fa8ca9ea6794325392b1da1
devtoc89 <devtoc89@gmail.com>
2024-11-22 22:34:44 +0900

    fix/manager-api: 서버기동 시 불필요 코드 삭제

--- apps/ai-trend-news-manager-api-server/src/app/index.ts
+++ apps/ai-trend-news-manager-api-server/src/app/index.ts
@@ -1,14 +1,14 @@
-import getServerInstance from "#app/server.ts";
-import server from "#app/server.ts";
+if (process.env.NODE_ENV === "development") {
+  require("dotenv").config();
+}
 
-// import server from "src/app/server.ts";
+import getServerInstance from "#app/server.ts";
 
 const start = async () => {
   const server = await getServerInstance();
   try {
     await server.listen({ port: 3000 });
     console.log(server.printRoutes());
-    // server.log.info("" + server.printRoutes() + "");
     server.log.info("server is listening");
   } catch (err) {
     server.log.error(err);
@@ -17,5 +17,3 @@ const start = async () => {
 };
 
 start();
-
-export default server;
