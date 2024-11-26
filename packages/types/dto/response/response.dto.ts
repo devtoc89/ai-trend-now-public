d61de14a84b53a9feaf35fbb61f3a68e30e7a0c8
devtoc89 <devtoc89@gmail.com>
2024-11-26 23:31:35 +0900

    chore/types: 내부 처리간 에러 발생 시, 에러 객체도 넘겨줄 수 있도록 정의

--- packages/types/dto/response/response.dto.ts
+++ packages/types/dto/response/response.dto.ts
@@ -1,1 +1,1 @@
-export type ResponseTypeDTO<T> = { success: true; data: T } | { success: false; error: string };
+export type ResponseTypeDTO<T> = { success: true; data: T } | { success: false; error: string; errorObj?: Error };
