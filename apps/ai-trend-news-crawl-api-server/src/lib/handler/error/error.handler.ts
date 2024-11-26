ca52695295926809bd78176e6d58f131a7092757
devtoc89 <devtoc89@gmail.com>
2024-11-26 23:34:09 +0900

    fix/crawler: 에러 메세지 버그(pino 2번째 인자에 입력하여 에러 메세지가 안떳음) fix

--- apps/ai-trend-news-crawl-api-server/src/lib/handler/error/error.handler.ts
+++ apps/ai-trend-news-crawl-api-server/src/lib/handler/error/error.handler.ts
@@ -2,6 +2,7 @@ import { logger } from "#lib/instance/logger/pino.instance.ts";
 
 // 통합 에러 핸들러 예시
 export function handleServiceError(error: Error, context: string) {
-  logger.error(`[${context}] Error:`, error.message);
+  logger.error(`[${context}] Error: ${error.message}`);
+  logger.error(error.stack);
   // TODO: 로깅, 알림 또는 재시도 로직 추가
 }
