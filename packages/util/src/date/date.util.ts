007ec905dcc47d6071baaa2983961dcb26e6b0ab
devtoc89 <devtoc89@gmail.com>
2024-11-26 23:32:49 +0900

    chore/util: date유틸에 dateFormat함수 추가

--- packages/util/src/date/date.util.ts
+++ packages/util/src/date/date.util.ts
@@ -1,7 +1,37 @@
+import dayjs from "dayjs";
+
 export function getCurrentTimeISOString(): string {
   return new Date().toISOString();
 }
 
 export function getElapsedTime(startTime: Date): number {
   return new Date().getTime() - startTime.getTime();
+}
+
+export function dateFormat({
+  value,
+  format = "YYYY-MM-DD",
+  defaultValue = "",
+}: {
+  value?: Date | string | null;
+  format?: string;
+  defaultValue?: string;
+}): string {
+  try {
+    if (!value) return defaultValue;
+    return datejsFormat({ value: dayjs(value), format, defaultValue });
+  } catch (_) {
+    return defaultValue;
+  }
+}
+
+export function datejsFormat({
+  value,
+  format = "YYYY-MM-DD",
+}: {
+  value: dayjs.Dayjs;
+  format?: string;
+  defaultValue?: string;
+}): string {
+  return dayjs(value).format(format);
 }
