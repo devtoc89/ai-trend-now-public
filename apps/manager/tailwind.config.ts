e3e700d8117fb494c8336222ef53bd1943c7fcf5
devtoc89 <devtoc89@gmail.com>
2024-12-09 15:20:19 +0900

    chore/manager: 불필요 console코드 제거

--- apps/manager/tailwind.config.ts
+++ apps/manager/tailwind.config.ts
@@ -1,21 +1,6 @@
 import path from "node:path";
 import type { Config } from "tailwindcss";
-console.log(path.join(path.dirname(require.resolve("@repo/ui"))));
-console.log(path.join(path.dirname(require.resolve("@repo/ui"))));
-console.log(path.join(path.dirname(require.resolve("@repo/ui"))));
-console.log(path.join(path.dirname(require.resolve("@repo/ui"))));
-console.log(path.join(path.dirname(require.resolve("@repo/ui"))));
-console.log(path.join(path.dirname(require.resolve("@repo/ui"))));
-console.log(path.join(path.dirname(require.resolve("@repo/ui"))));
-console.log(path.join(path.dirname(require.resolve("@repo/ui"))));
-console.log(path.join(path.dirname(require.resolve("@repo/ui"))));
-console.log(path.join(path.dirname(require.resolve("@repo/ui"))));
-console.log(path.join(path.dirname(require.resolve("@repo/ui"))));
-console.log(path.join(path.dirname(require.resolve("@repo/ui"))));
-console.log(path.join(path.dirname(require.resolve("@repo/ui"))));
-console.log(path.join(path.dirname(require.resolve("@repo/ui"))));
-console.log(path.join(path.dirname(require.resolve("@repo/ui"))));
-console.log(path.join(path.dirname(require.resolve("@repo/ui"))));
+
 const config: Config = {
   content: [
     "./src/layer/ui/**/*.{js,ts,jsx,tsx,mdx}",
