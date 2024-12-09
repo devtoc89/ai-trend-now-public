8eba0aede3b71c6c17f1fda45ce81db1ab24dfba
devtoc89 <devtoc89@gmail.com>
2024-12-09 15:40:38 +0900

    feat/user: AI 기사글 경고 및 원글 작성 시간, 작성자 등 원글에 대한 서술 추가 및 하단 뒤로가기 버튼 추가

--- apps/user/src/layer/ui/pages/PostDetailPage/(server)/PostDetailPageContent.tsx
+++ apps/user/src/layer/ui/pages/PostDetailPage/(server)/PostDetailPageContent.tsx
@@ -1,10 +1,41 @@
 "use server";
 
-import type { PostItemViewItem } from "#layer/domain/post/post.action";
+import type { PostItemViewItem } from "#layer/action/post.action";
 import PageBackButton from "#layer/ui/components/common/(client)/PageBack/PageBackButton";
+import Time from "#layer/ui/components/common/(client)/Time/TimeAgo";
 import TimeAgo from "#layer/ui/components/common/(client)/TimeAgo/TimeAgo";
 import Markdown from "#layer/ui/components/common/(server)/Markdown/Markdown";
+import type { AiContentReference } from "@repo/types/model/ai.model";
 import Image from "next/image";
+
+function ReferenceItem({ item }: { item: AiContentReference }) {
+  return (
+    <div className="whitespace-normal pt-1" key={item.url}>
+      <p>
+        <strong>[{item.source}]</strong> {item.title}
+      </p>
+      <p className="text-sm">
+        <strong>Published: </strong>
+        {item.createdAt && <Time>{item.createdAt ?? ""}</Time>}
+        {item.lastUpdatedAt && (
+          <>
+            &nbsp;(Updated: <Time>{item.lastUpdatedAt ?? ""}</Time>)
+          </>
+        )}
+      </p>
+      {item.authors && (
+        <p className="text-sm">
+          <strong>Author: </strong>
+          {Array.isArray(item.authors) ? item.authors.map((v) => v.name).join(", ") : item.authors.name}
+        </p>
+      )}
+
+      <a href={item.url} target="_blank" rel="noreferrer" className="underline">
+        {item.url}
+      </a>
+    </div>
+  );
+}
 
 function PostDetailPageContent({ post }: { post: PostItemViewItem }) {
   return (
@@ -31,20 +62,21 @@ function PostDetailPageContent({ post }: { post: PostItemViewItem }) {
           </div>
           <Markdown>{post.content}</Markdown>
           <br />
+          <div className="text-sm font-bold text-red-800">
+            *이 기사는 AI가 생성한 내용으로, 일부 정보가 실제와 다를 수 있습니다. 정확한 확인을 위해 추가적인 검증을
+            권장드립니다.
+          </div>
+          <br />
           <div className="">
             <h3>Reference</h3>
             {post.metadata?.references.map((v) => (
-              <div className="whitespace-normal pt-1" key={v.url}>
-                <p>
-                  <strong>[{v.source}]</strong> {v.title}
-                </p>
-                <a href={v.url} target="_blank" rel="noreferrer" className="underline">
-                  {v.url}
-                </a>
-              </div>
+              <ReferenceItem item={v} key={v.url} />
             ))}
           </div>
         </div>
+      </div>
+      <div className="flex flex-row justify-end">
+        <PageBackButton variant="ghost" />
       </div>
     </div>
   );
