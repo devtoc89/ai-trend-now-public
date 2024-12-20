e4efefcb7521ce83cff1e76dd5efefabaab10db7
devtoc89 <devtoc89@gmail.com>
2024-12-20 10:59:22 +0900

    feat/user: pagination 너비 100%에서 개별 컨텐츠 사이즈에 맞추도록 변경경

--- apps/user/src/layer/ui/pages/PostListPage/(server)/PostListPagePagination.tsx
+++ apps/user/src/layer/ui/pages/PostListPage/(server)/PostListPagePagination.tsx
@@ -36,14 +36,15 @@ function PostPagePagination({
 
   return (
     <Pagination className="h-full w-full">
-      <PaginationContent className="justify-between w-full">
+      <PaginationContent className="justify-between w-fit gap-4">
         <PaginationItem>
           <PaginationLink
             href={currentPage === prevPage ? "#" : `${urlPath}page/${prevPage + 1}`}
             size="default"
             prefetch
+            aria-label="previous pages"
             aria-disabled={currentPage === prevPage}
-            className={cn(currentPage === prevPage && "pointer-events-none", "p-0 px-2")}
+            className={cn(currentPage === prevPage && "pointer-events-none", "p-0 px-4")}
           >
             <ChevronLeft className="h-4 w-4" />
           </PaginationLink>
@@ -56,9 +57,9 @@ function PostPagePagination({
               size="default"
               prefetch
               aria-disabled={currentPage === page}
-              className={cn(currentPage === page && "pointer-events-none", "p-0 px-2")}
+              className={cn(currentPage === page && "pointer-events-none", "p-0 px-0")}
             >
-              {page + 1}
+              <p className="w-10 flex justify-center">{page + 1}</p>
             </PaginationLink>
           </PaginationItem>
         ))}
@@ -67,8 +68,9 @@ function PostPagePagination({
             href={currentPage === nextPage ? "#" : `${urlPath}page/${nextPage + 1}`}
             size="default"
             prefetch
+            aria-label="next pages"
             aria-disabled={currentPage === nextPage}
-            className={cn(currentPage === nextPage && "pointer-events-none", "p-0 px-2")}
+            className={cn(currentPage === nextPage && "pointer-events-none", "p-0 px-4")}
           >
             <ChevronRight className="h-4 w-4" />
           </PaginationLink>
