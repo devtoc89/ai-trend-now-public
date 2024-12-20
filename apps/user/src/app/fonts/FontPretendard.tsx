c7b36ac7db54f505b80f238ffbf19c2171e0b309
devtoc89 <devtoc89@gmail.com>
2024-12-20 10:55:32 +0900

    feat/user: LCP개선을 위한 폰트 제한한

--- apps/user/src/app/fonts/FontPretendard.tsx
+++ apps/user/src/app/fonts/FontPretendard.tsx
@@ -2,14 +2,14 @@ import localFont from "next/font/local";
 
 const FontPretendard = localFont({
   src: [
-    {
-      path: "./woff2/Pretendard-Black.woff2",
-      weight: "900",
-    },
-    {
-      path: "./woff2/Pretendard-ExtraBold.woff2",
-      weight: "800",
-    },
+    // {
+    //   path: "./woff2/Pretendard-Black.woff2",
+    //   weight: "900",
+    // },
+    // {
+    //   path: "./woff2/Pretendard-ExtraBold.woff2",
+    //   weight: "800",
+    // },
     {
       path: "./woff2/Pretendard-Bold.woff2",
       weight: "700",
@@ -18,26 +18,26 @@ const FontPretendard = localFont({
       path: "./woff2/Pretendard-SemiBold.woff2",
       weight: "600",
     },
-    {
-      path: "./woff2/Pretendard-Medium.woff2",
-      weight: "500",
-    },
+    // {
+    //   path: "./woff2/Pretendard-Medium.woff2",
+    //   weight: "500",
+    // },
     {
       path: "./woff2/Pretendard-Regular.woff2",
       weight: "400",
     },
-    {
-      path: "./woff2/Pretendard-Light.woff2",
-      weight: "300",
-    },
-    {
-      path: "./woff2/Pretendard-ExtraLight.woff2",
-      weight: "200",
-    },
-    {
-      path: "./woff2/Pretendard-Thin.woff2",
-      weight: "100",
-    },
+    // {
+    //   path: "./woff2/Pretendard-Light.woff2",
+    //   weight: "300",
+    // },
+    // {
+    //   path: "./woff2/Pretendard-ExtraLight.woff2",
+    //   weight: "200",
+    // },
+    // {
+    //   path: "./woff2/Pretendard-Thin.woff2",
+    //   weight: "100",
+    // },
   ],
   preload: true,
   variable: "--font-pretendard",
