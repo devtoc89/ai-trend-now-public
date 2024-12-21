8a1362fc29327765a304aab107166a6f94a509e0
devtoc89 <devtoc89@gmail.com>
2024-12-21 18:26:28 +0900

    feat/user: next.config.ts에 bundle analyzer 설정 추가

--- apps/user/next.config.ts
+++ apps/user/next.config.ts
@@ -1,22 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // webpack: (config, { dev }) => {
-  //   if (dev) {
-  //     config.devtool = "source-map";
-  //   }
-  //   return config;
-  // },
-  // reactStrictMode: false,
-  /* config options here */
-  // serverRuntimeConfig: {},
   eslint: {
     ignoreDuringBuilds: true,
   },
-  transpilePackages: ['next-mdx-remote'],
 };
 
+function getNextConfig() {
+  if (process.env.ANALYZE !== "true" || process.env.NODE_ENV === "production") {
+    return nextConfig;
+  }
+  const withBundleAnalyzer = require("@next/bundle-analyzer")({
+    enabled: process.env.ANALYZE === "true",
+  });
+  return withBundleAnalyzer(nextConfig);
+}
 
-
-
-export default nextConfig
+export default getNextConfig();
