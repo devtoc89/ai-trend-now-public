f63764ecbdf9e86f89156af4b3da65e95ceaeccb
devtoc89 <devtoc89@gmail.com>
2024-12-27 09:13:13 +0900

    minor/manager: BATCH키 적용용

--- apps/manager/src/app/api/(private)/post/publish/route.ts
+++ apps/manager/src/app/api/(private)/post/publish/route.ts
@@ -1,7 +1,15 @@
-import { autoPublishByCategory } from "#layer/service/publish/publish.service";
 import type { PostCategoryEnum } from "@repo/types/enums/post.category.enum";
+import { autoPublishByCategory } from "#layer/service/publish/publish.service";
 
 export async function POST(req: Request) {
+  const batchKey = req.headers.get("X-BATCH-AUTH-KEY");
+
+  if (batchKey !== process.env.BATCH_KEY) {
+    return new Response("{}", {
+      status: 403,
+    });
+  }
+
   const { postCategoryList, aiPostCategory } = (await req.json()) as {
     postCategoryList: PostCategoryEnum[];
     aiPostCategory: PostCategoryEnum;
