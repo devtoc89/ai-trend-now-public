7c8f25bcd86c817f7d545ff7f4db0c0fd51d00f1
devtoc89 <devtoc89@gmail.com>
2024-12-29 17:35:28 +0900

    feat/manager: build는 standalone으로 진행되도록함함

--- apps/manager/next.config.ts
+++ apps/manager/next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // webpack: (config, { dev }) => {
-  //   if (dev) {
-  //     config.devtool = "source-map";
-  //   }
-  //   return config;
-  // },
-  // reactStrictMode: false,
-  /* config options here */
+  output: "standalone",
   eslint: {
     ignoreDuringBuilds: true,
   },
