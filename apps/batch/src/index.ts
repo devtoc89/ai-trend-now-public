ef92824f437b4b86fda2b0c5ba0f963bf9d3c71a
devtoc89 <devtoc89@gmail.com>
2024-12-29 21:48:54 +0900

    chore/batch: dotenv 처리를 기본으로 하도록 함함

--- apps/batch/src/index.ts
+++ apps/batch/src/index.ts
@@ -1,6 +1,4 @@
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
+require("dotenv").config();
 
 import { jobRegister } from "#helper/job.helper";
 import { arxivAiPostJob } from "#jobs/arxivAiPostJob";
